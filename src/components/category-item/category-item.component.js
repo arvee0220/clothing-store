0fb6e853d62ae738da07d4bc64049741fea96d04
Rowell Verdejo <rowellverdejo@gmail.com>
2023-06-27 19:59:09 +0800

    Add categories-preview component and revised shop component

--- src/components/category-item/category-item.component.js
+++ src/components/category-item/category-item.component.js
@@ -1,14 +1,18 @@
-import "./category-item.styles.scss";
+import { useNavigate } from 'react-router-dom';
+import './category-item.styles.scss';
 
 const CategoryItem = ({ category }) => {
     const { imageUrl, title } = category;
+    const navigate = useNavigate();
+
+    const goToShopHandler = () => {
+        navigate('/shop');
+    };
+
     return (
-        <div className="category-container">
-            <div
-                className="background-image"
-                style={{ backgroundImage: `url(${imageUrl})` }}
-            />
-            <div className="category-body-container">
+        <div className='category-container' onClick={goToShopHandler}>
+            <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
+            <div className='category-body-container'>
                 <h2>{title}</h2>
                 <p>Shop Now</p>
             </div>
--- src/routes/shop/shop.component.js
+++ src/routes/shop/shop.component.js
@@ -1,27 +1,13 @@
-import { useContext } from 'react';
-
-import { CategoriesContext } from '../../context/categories.context';
-import ProductCard from '../../components/product-card/product-card.component';
+import { Routes, Route } from 'react-router-dom';
 
 import '../shop/shop.styles.scss';
+import CategoriesPreview from '../categories-preview/categories-preview.compoent';
 
 const Shop = () => {
-    const { categoriesMap } = useContext(CategoriesContext);
     return (
-        <>
-            {Object.keys(categoriesMap).map((title) => {
-                return (
-                    <div key={title}>
-                        <h2>{title}</h2>
-                        <div className='products-container'>
-                            {categoriesMap[title].map((product) => (
-                                <ProductCard key={product.id} product={product} />
-                            ))}
-                        </div>
-                    </div>
-                );
-            })}
-        </>
+        <Routes>
+            <Route index element={<CategoriesPreview />} />
+        </Routes>
     );
 };
 
