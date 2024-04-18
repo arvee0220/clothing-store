1519efee6b74b0288b27552a798d2a59900282a1
arvee0220 <rowellverdejo@gmail.com>
2024-04-17 21:05:45 -0400

    centralized setCurrentUser hook by implementing onAuthStateChanged from firebase/auth

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,8 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import {
+    createUserDocumentFromAuth,
+    onAuthStateChangedListener,
+} from "../utils/firebase/firebase.utils";
 import PropTypes from "prop-types";
 
 // as the actual value you want to access
@@ -10,6 +14,18 @@ export const UserContext = createContext({
 export const UserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
     const value = { currentUser, setCurrentUser };
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedListener((user) => {
+            if (user) {
+                createUserDocumentFromAuth(user);
+            }
+
+            setCurrentUser(user);
+        });
+
+        return unsubscribe;
+    }, []);
 
     return (
         <UserContext.Provider value={value}>{children}</UserContext.Provider>
