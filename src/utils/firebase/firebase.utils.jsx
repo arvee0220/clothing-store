10353e71a500262c97aa9e326dc91816b0f1e660
arvee0220 <rowellverdejo@gmail.com>
2024-04-23 08:03:54 -0400

    create a function to upload data collection in firebase

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -8,7 +8,14 @@ import {
     signOut,
     onAuthStateChanged,
 } from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+    collection,
+    writeBatch,
+} from "firebase/firestore";
 
 // Set-up and connect to firebase database
 const firebaseConfig = {
@@ -84,3 +91,20 @@ export const signOutUser = async () => await signOut(auth);
 
 export const onAuthStateChangedListener = (callback) =>
     onAuthStateChanged(auth, callback);
+
+// Upload collection
+export const addCollectionAndDocuments = async (
+    collectionKey,
+    objectsToAdd
+) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach((object) => {
+        const docRef = doc(collectionRef, object.title.toLowerCase());
+
+        batch.set(docRef, object);
+    });
+    await batch.commit();
+    console.log("done");
+};
