f2efe10c6dbd2db9ea8643fa01298aab4f2957b4
arvee0220 <rowellverdejo@gmail.com>
2024-04-23 20:25:45 -0400

    Update the shop component to map the data in firebase

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,8 +4,21 @@ import ProductCard from "../../components/product-card/product-card.component";
 import "./shop.styles.scss";
 
 const Shop = () => {
-    const {} = useContext(CategoriesContext);
-    return <div className="products-container"></div>;
+    const { categoriesMap } = useContext(CategoriesContext);
+    return (
+        <>
+            {Object.keys(categoriesMap).map((title) => (
+                <div key={title}>
+                    <h2>{title}</h2>
+                    <div className="products-container">
+                        {categoriesMap[title].map((product) => (
+                            <ProductCard key={product.id} product={product} />
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </>
+    );
 };
 
 export default Shop;
