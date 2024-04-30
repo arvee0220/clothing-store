76ddccad9b6931e1d203fced4fc1610347f34eb8
arvee0220 <rowellverdejo@gmail.com>
2024-04-30 18:33:42 -0400

    Implement redux-persist

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,10 +1,44 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./root-reducer";
+import {
+    persistStore,
+    persistReducer,
+    REHYDRATE,
+    PURGE,
+    REGISTER,
+    FLUSH,
+    PAUSE,
+    PERSIST,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
+
+// redux-persist
+const persistConfig = {
+    key: "root", // The key for the persist
+    storage, // The storage to use
+    blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // root-reducer
 export const store = configureStore({
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }).concat(logger),
     preloadedState: {},
 });
+
+export const persistor = persistStore(store);
