94cdd9c27c404faad13345f2a7f79b9dab0257d4
arvee0220 <rowellverdejo@gmail.com>
2024-05-11 16:21:21 -0400

    install stripe

--- src/store/categories/category.slice.jsx
+++ src/store/categories/category.slice.jsx
@@ -17,39 +17,3 @@ export const categoriesSlice = createSlice({
 export const { setCategories } = categoriesSlice.actions;
 
 export const categoriesReducer = categoriesSlice.reducer;
-
-/* export const categoriesReducer = (
-    state = CATEGORIES_INITIAL_STATE,
-    action = {}
-) => {
-    const { type, payload } = action;
-
-    const {
-        FETCH_CATEGORIES_START,
-        FETCH_CATEGORIES_SUCCESS,
-        FETCH_CATEGORIES_FAILED,
-    } = CATEGORY_ACTION_TYPES;
-
-    switch (type) {
-        case FETCH_CATEGORIES_START:
-            return {
-                ...state,
-                isLoading: true,
-            };
-        case FETCH_CATEGORIES_SUCCESS:
-            return {
-                ...state,
-                isLoading: false,
-                categories: payload,
-            };
-        case FETCH_CATEGORIES_FAILED:
-            return {
-                ...state,
-                isLoading: false,
-                error: payload,
-            };
-        default:
-            return state;
-    }
-};
- */
