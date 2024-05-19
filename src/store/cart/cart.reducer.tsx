14b436f99f89a60a19adbb235a5ca888f52e704e
arvee0220 <rowellverdejo@gmail.com>
2024-05-19 13:50:57 -0400

    fix cart reducer cart toggling function

--- src/store/cart/cart.reducer.tsx
+++ src/store/cart/cart.reducer.tsx
@@ -1,4 +1,4 @@
-import { UnknownAction } from "@reduxjs/toolkit";
+import { Action, PayloadAction } from "@reduxjs/toolkit";
 import { CartItem } from "./cart.types";
 import { setCartItems, toggleCart } from "./cart.action";
 
@@ -12,10 +12,14 @@ const CART_INITIAL_STATE: CartState = {
 	isCartOpen: false,
 };
 
-export const cartReducer = (state = CART_INITIAL_STATE, action: UnknownAction): CartState => {
-	if (toggleCart.match(action)) ({ ...state, isCartOpen: action.payload });
+export const cartReducer = (state = CART_INITIAL_STATE, action: Action<string>): CartState => {
+	if (toggleCart.match(action)) {
+		return { ...state, isCartOpen: (action as PayloadAction<boolean>).payload };
+	}
 
-	if (setCartItems.match(action)) ({ ...state, cartItems: action.payload });
+	if (setCartItems.match(action)) {
+		return { ...state, cartItems: (action as PayloadAction<CartItem[]>).payload };
+	}
 
 	return state;
 };
--- src/store/categories/category.saga.tsx
+++ src/store/categories/category.saga.tsx
@@ -1,31 +1,25 @@
-import { takeLatest, all, call, put } from "redux-saga/effects";
+import { takeLatest, all, call, put } from "typed-redux-saga";
 
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
-import {
-    fetchCategoriesSuccess,
-    fetchCategoriesFailed,
-} from "./category.action";
+import { fetchCategoriesSuccess, fetchCategoriesFailed } from "./category.action";
 import { CATEGORY_ACTION_TYPES } from "./category.types";
 
 const { FETCH_CATEGORIES_START } = CATEGORY_ACTION_TYPES;
 
 export const fetchCategoriesAsync = function* () {
-    try {
-        const categoriesArray = yield call(
-            getCategoriesAndDocuments,
-            "categories"
-        );
-        yield put(fetchCategoriesSuccess(categoriesArray));
-    } catch (error) {
-        yield put(fetchCategoriesFailed(error));
-    }
+	try {
+		const categoriesArray = yield* call(getCategoriesAndDocuments, "categories");
+		yield* put(fetchCategoriesSuccess(categoriesArray));
+	} catch (error) {
+		yield* put(fetchCategoriesFailed(error));
+	}
 };
 
 export const onFetchCategories = function* () {
-    yield takeLatest(FETCH_CATEGORIES_START, fetchCategoriesAsync);
+	yield* takeLatest(FETCH_CATEGORIES_START, fetchCategoriesAsync);
 };
 
 export const categoriesSaga = function* () {
-    yield all([call(onFetchCategories)]);
+	yield* all([call(onFetchCategories)]);
 };
