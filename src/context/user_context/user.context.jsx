3648580cf0547530410b4ac992b1ee066df6683f
arvee0220 <rowellverdejo@gmail.com>
2024-05-21 18:00:26 -0400

    remove use reducer

--- src/context/user_context/user.context.jsx
+++ src/context/user_context/user.context.jsx
@@ -1,58 +1,30 @@
-import { createContext, useEffect, useReducer } from "react";
+import { createContext, useEffect, useState } from "react";
 import {
-    createUserDocumentFromAuth,
-    onAuthStateChangedListener,
+	createUserDocumentFromAuth,
+	onAuthStateChangedListener,
 } from "../../utils/firebase/firebase.utils";
-import { USER_ACTION_TYPES } from "../../reducerActionTypes/userActionTypes";
 
 // as the actual value you want to access
 export const UserContext = createContext({
-    currentUser: null,
-    setCurrentUser: () => null,
+	currentUser: null,
+	setCurrentUser: () => null,
 });
 
-const INITIAL_STATE = {
-    currentUser: null,
-};
-
-const userReducer = (state, action) => {
-    const { type, payload } = action;
-
-    switch (type) {
-        case USER_ACTION_TYPES.SET_CURRENT_USER:
-            return {
-                ...state,
-                currentUser: payload,
-            };
-        default:
-            throw new Error(`Unhandled type ${type} in userReducer`);
-    }
-};
-
 export const UserProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+	const [currentUser, setCurrentUser] = useState(null);
+	const value = { currentUser, setCurrentUser };
 
-    const { currentUser } = state;
+	useEffect(() => {
+		const unsubscribe = onAuthStateChangedListener((user) => {
+			if (user) {
+				createUserDocumentFromAuth(user);
+			}
 
-    const setCurrentUser = (user) => {
-        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
-    };
+			setCurrentUser(user);
+		});
 
-    const value = { currentUser, setCurrentUser };
+		return unsubscribe;
+	}, []);
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChangedListener((user) => {
-            if (user) {
-                createUserDocumentFromAuth(user);
-            }
-
-            setCurrentUser(user);
-        });
-
-        return unsubscribe;
-    }, []);
-
-    return (
-        <UserContext.Provider value={value}>{children}</UserContext.Provider>
-    );
+	return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
