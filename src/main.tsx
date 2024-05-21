1698fb966a715acd1ea2c1828fc4d88de044b749
arvee0220 <rowellverdejo@gmail.com>
2024-05-21 08:19:44 -0400

    Modify index.html

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import App from "./App.js";
+import App from "./App";
 
 import "./index.scss";
 
