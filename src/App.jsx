09f9caefa8a4150894fdfb958c2a2d6a3706d1f4
arvee0220 <rowellverdejo@gmail.com>
2024-05-27 11:52:21 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,40 @@
-import React from 'react';
-
 const App = () => {
-    const categories = [
-        {
-            id: 1,
-            title: 'Hats',
-        },
-        {
-            id: 2,
-            title: 'Jackets',
-        },
-        {
-            id: 3,
-            title: 'Sneakers',
-        },
-        {
-            id: 4,
-            title: 'Womens',
-        },
-        {
-            id: 5,
-            title: 'Mens',
-        },
-    ];
+	const categories = [
+		{
+			id: 1,
+			title: "Hats",
+		},
+		{
+			id: 2,
+			title: "Jackets",
+		},
+		{
+			id: 3,
+			title: "Sneakers",
+		},
+		{
+			id: 4,
+			title: "Womens",
+		},
+		{
+			id: 5,
+			title: "Mens",
+		},
+	];
 
-    return (
-        <div className='categories-container'>
-            {categories.map(({ title }) => (
-                <div className='category-container'>
-                    <div className='background-image' />
-                    <div className='category-body-container'>
-                        <h2>{title}</h2>
-                        <p>Shop Now</p>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
+	return (
+		<div className="categories-container">
+			{categories.map(({ title, id }) => (
+				<div key={id} className="category-container">
+					<div className="background-image" />
+					<div className="category-body-container">
+						<h2>{title}</h2>
+						<p>Shop Now</p>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default App;
