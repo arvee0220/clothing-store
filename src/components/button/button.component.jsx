5aabfca6fa5f965961a39f6c1973a60f0bfe494e
arvee0220 <rowellverdejo@gmail.com>
2024-06-06 18:41:17 -0400

    add button spinner to button component

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,20 +1,22 @@
+import ButtonSpinner from "./button-spinner.component";
 import "./button.styles.scss";
 
 const BUTTON_TYPE_CLASSES = {
-    google: "button-container google-sign-in",
-    inverted: "button-container inverted",
-    default: "button-container",
+	google: "button-container google-sign-in",
+	inverted: "button-container inverted",
+	default: "button-container",
 };
 
-const Button = ({ children, buttonType, ...otherProps }) => {
-    return (
-        <button
-            className={`${BUTTON_TYPE_CLASSES[buttonType]}`}
-            {...otherProps}
-        >
-            {children}
-        </button>
-    );
+const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
+	return (
+		<button
+			disabled={isLoading}
+			className={`${BUTTON_TYPE_CLASSES[buttonType]}`}
+			{...otherProps}
+		>
+			{isLoading ? <ButtonSpinner /> : children}
+		</button>
+	);
 };
 
 export default Button;
