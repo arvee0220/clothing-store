daeb97bb411fbff01cbfda68fad32d37fb5753e0
arvee0220 <rowellverdejo@gmail.com>
2024-06-06 18:28:00 -0400

    remove saga files

--- src/store/root-saga.jsx
+++
@@ -1,3 +0,0 @@
-import { all, call } from "redux-saga/effects";
-
-export const rootSaga = function* () {};
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,25 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./root-reducer";
 import {
-    persistStore,
-    persistReducer,
-    REHYDRATE,
-    PURGE,
-    REGISTER,
-    FLUSH,
-    PAUSE,
-    PERSIST,
+	persistStore,
+	persistReducer,
+	REHYDRATE,
+	PURGE,
+	REGISTER,
+	FLUSH,
+	PAUSE,
+	PERSIST,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 import createSagaMiddleware from "redux-saga";
-import { rootSaga } from "./root-saga";
 
 // redux-persist
 const persistConfig = {
-    key: "root", // The key for the persist
-    storage, // The storage to use
-    blacklist: ["user"],
+	key: "root", // The key for the persist
+	storage, // The storage to use
+	blacklist: ["user"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -29,26 +28,16 @@ const sagaMiddleware = createSagaMiddleware();
 
 // root-reducer
 export const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [
-                    FLUSH,
-                    REHYDRATE,
-                    PAUSE,
-                    PERSIST,
-                    PURGE,
-                    REGISTER,
-                ],
-            },
-        })
-            .concat(import.meta.env.VITE_ENV !== "production" ? logger : [])
-            .concat(sagaMiddleware),
-    preloadedState: {},
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		})
+			.concat(import.meta.env.VITE_ENV !== "production" ? logger : [])
+			.concat(sagaMiddleware),
+	preloadedState: {},
 });
-
-// Run the saga
-sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
